751a50189c82e076510ae5993374f428a12547aa
scarlhos <josevillares03@gmail.com>
2023-07-04 16:35:30 -0500

    first commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+
+
+/*!function(e){var t={}; function n(r){if(t[r])return t[r] .exports}}*/
+
+console.log("Hola mundo!")
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<h1>'Programacion Avanzada PUCESE'</h1>,document.getElementById('app'))
